675ebe63dec902e9f033f2afdb1df00c1ba8e85c
mehulgarg1211 <167665368+mehulgarg1211@users.noreply.github.com>
2024-04-21 17:18:09 +0530

    Update app.js

--- app.js
+++ app.js
@@ -4,9 +4,9 @@ const content = document.querySelector('.content');
 function speak(text) {
     const text_speak = new SpeechSynthesisUtterance(text);
 
-    text_speak.rate = 2;
-    text_speak.volume = 2;
-    text_speak.pitch = 2;
+    text_speak.rate = 1;
+    text_speak.volume = 1;
+    text_speak.pitch = 1;
 
     window.speechSynthesis.speak(text_speak);
 }
